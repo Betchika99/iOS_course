a06b9d73f4ac4621d5869bfeffc6cfd9b011a96b
Betchika99 <dobrl2011@mail.ru>
2018-09-26 23:29:41 +0300

    Started new app

---
+++ hw1/hw1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hw1
+//
+//  Created by Елизавета Добрянская on 26.09.2018.
+//  Copyright © 2018 Елизавета Добрянская. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
